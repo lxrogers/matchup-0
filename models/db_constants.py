8cb1cec15c2a644dc3ba18b368f81672a0ef4a97
tdliu <tliu.auckland@gmail.com>
2016-03-27 22:21:04 +0000

    Full combine anthro script and adding constants file

---
+++ models/db_constants.py
@@ -0,0 +1,5 @@
+# db_constants.py
+
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/601.4.4 (KHTML, like Gecko) Version/9.0.3 Safari/601.4.4'
+STATSNBA_SEASONYEAR = ["2000-01", "2001-02", "2002-03", "2003-04", "2004-05", "2005-06", "2006-07", "2007-08", 
+                        "2008-09", "2009-10", "2010-11", "2011-12", "2012-13", "2013-14", "2014-15", "2015-16"]
--- models/dev-update.py
+++ models/dev-update.py
@@ -3,31 +3,51 @@
 import requests
 import psycopg2
 from operator import itemgetter
-
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/601.4.4 (KHTML, like Gecko) Version/9.0.3 Safari/601.4.4'
-
-# Connect to data base
-conn_string = "host='localhost' dbname='development' user='postgres' password='steph43'"
-conn = psycopg2.connect(conn_string)
-cursor = conn.cursor()
+import db_constants
 
 anthro_features = ("FIRST_NAME", "LAST_NAME", "PLAYER_NAME", "HEIGHT_WO_SHOES", "HEIGHT_W_SHOES", "WEIGHT", "WINGSPAN", "STANDING_REACH", "BODY_FAT_PCT", "HAND_LENGTH", "HAND_WIDTH")
 
 # Create a temporary table for each separate data source, merge tables, and then drop temporary tables
 # Not supposed to use %?
-cursor.execute('DROP TABLE IF EXISTS temp_combine_anthro')
 
-cursor.execute('CREATE TABLE temp_combine_anthro (%s varchar(20) NOT NULL, %s varchar(40) NOT NULL, %s varchar(60), %s numeric(4,2), %s numeric(4,2), %s numeric(5,2), %s numeric(4,2), %s numeric(5,2), %s numeric(4,2), %s numeric(4,2), %s numeric(4,2));' % anthro_features)
+def create_combine_anthro_table(cursor):
+    cursor.execute("""CREATE TABLE temp_combine_anthro (%s varchar(20) NOT NULL, %s varchar(40) NOT NULL, 
+                        %s varchar(60), %s numeric(4,2), %s numeric(4,2), %s numeric(5,2), %s numeric(4,2), 
+                        %s numeric(5,2), %s numeric(4,2), %s numeric(4,2), %s numeric(4,2));""" % anthro_features)
+    return 0
+    
+def drop_combine_anthro_table(cursor):
+    cursor.execute('DROP TABLE IF EXISTS temp_combine_anthro')
+    return 0
+    
+def insert_combine_anthro_table(cursor, seasonyear_str):
+    requests_headers = {'user-agent': db_constants.USER_AGENT}
+    combine_anthro_url = "http://stats.nba.com/stats/draftcombineplayeranthro?LeagueID=00&SeasonYear=%s" % seasonyear_str
+    response = requests.get(combine_anthro_url, headers = requests_headers)
+    headers = response.json()['resultSets'][0]['headers']
+    anthro_feature_index = [i for i,anthro_feature in enumerate(headers) if anthro_feature in anthro_features]
+    player_anthro_stats = response.json()['resultSets'][0]['rowSet']
+    for row in player_anthro_stats:
+        cursor.execute("""INSERT INTO temp_combine_anthro VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", itemgetter(*anthro_feature_index)(row))
+    return 0
 
-requests_headers = {'user-agent': USER_AGENT}
-combine_anthro_url = "http://stats.nba.com/stats/draftcombineplayeranthro?LeagueID=00&SeasonYear=2015-16"
-response = requests.get(combine_anthro_url, headers = requests_headers)
-headers = response.json()['resultSets'][0]['headers']
-anthro_feature_index = [i for i,anthro_feature in enumerate(headers) if anthro_feature in anthro_features]
-player_anthro_stats = response.json()['resultSets'][0]['rowSet']
-# Insert data in temporary postgres table
+def update_combine_anthro_table(cursor):
+    drop_combine_anthro_table(cursor)
+    create_combine_anthro_table(cursor)
+    for seasonyear_str in db_constants.STATSNBA_SEASONYEAR:
+        insert_combine_anthro_table(cursor, seasonyear_str)
+    return 0
+    
+def main():
+    # Connect to data base
+    conn_string = "host='localhost' dbname='development' user='postgres' password='steph43'"
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    print "Running"
+    #drop_combine_anthro_table(cursor)
+    update_combine_anthro_table(cursor)
+    conn.commit()
+    conn.close()
 
-for row in player_anthro_stats:
-    cursor.execute("""INSERT INTO temp_combine_anthro VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", itemgetter(*anthro_feature_index)(row))
-
-conn.commit()
+if __name__ == "__main__":
+    main()
