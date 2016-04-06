2fa859cbabf2409064316fb165669f750e834376
Lawrence Rogers <lawrence.x.rogers@gmail.com>
2016-04-06 18:05:07 +0000

    added tony database module

---
+++ tonydb.js
@@ -0,0 +1,18 @@
+var pg = require('pg-promise');
+
+var exports = module.exports = {};
+
+exports.newFunction = function(param1, param2) {
+  
+}
+
+//something like this:
+//I will know which players are on the court (name or SI id)
+// and I'll want to find out other things about that player
+// or lineup
+
+//server might request all of the data for that day
+
+exports.getPlayerData = function(players) {
+    
+}
