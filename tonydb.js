375db281b9fa5a2041ed55bd7e7a9cd58d55a123
tdliu <tliu.auckland@gmail.com>
2016-04-12 00:48:17 +0000

    Adding tonydb

--- tonydb.js
+++ tonydb.js
@@ -1,9 +1,20 @@
-var pg = require('pg-promise');
+var pgp = require('pg-promise')();
+
+var cn = {
+    host: 'localhost', // server name or IP address; 
+    port: 5432,
+    database: 'development',
+    user: 'postgres',
+    password: 'steph43'
+};
+
+var db = pgp(cn);
 
 var exports = module.exports = {};
 
-exports.newFunction = function(param1, param2) {
-  
+function printData(error, data) {
+    console.log("LAWRENCE CALLBACK: ")
+    console.log(data);
 }
 
 //something like this:
@@ -12,7 +23,30 @@ exports.newFunction = function(param1, param2) {
 // or lineup
 
 //server might request all of the data for that day
+var indexBy = function(array, property) {
+  var results = {};
+  (array||[]).forEach(function(object) {
+    results[object[property]] = object;
+  });
+  return results
+};
 
-exports.getPlayerData = function(players) {
-    
+exports.getPlayerData = function(player_names, callback) {
+    // make request to db
+    // store answer
+    // call callback function on answer
+    //callback(data);
+    db.any("SELECT * FROM player_data WHERE first_last in ($1^);", pgp.as.csv(player_names)).then(function (data) {
+        //do stuff to data
+        callback('success', indexBy(data, 'first_last'));
+    })
+    .catch(function (error) {
+        console.log("Error: " + error)
+        // error;
+    });
 }
+//this could be in my file
+var todays_players = ["Dwyane Wade", "LeBron James", "Kevin Durant"];
+
+exports.getPlayerData(todays_players, printData)
+pgp.end();
